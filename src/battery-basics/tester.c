8deff220494c2ae3c0366d9ec5c854d96a4b4332
mjhegarty <mhegarty@gwu.edu>
2019-11-11 03:41:46 +0000

    Got i2c to print the write value!

--- src/battery-basics/tester.c
+++ src/battery-basics/tester.c
@@ -24,17 +24,18 @@ void main(void) {
 		printf("ioctl error: %s\n", strerror(errno));
 		exit(1);
 	}
+	char buf[10];
+	buf[0] = 0x00;
+	buf[1] = 0x01;
+	buf[2] = 0x00;
+	buf[3] = 0x00;
 
-	uint8_t writes[2] = { 0x00, 0x01 };
-
-	write(file, &writes[0], 1);
-	write(file, &writes[1], 1);
-	write(file, &writes[0], 1);
-	write(file, &writes[1], 1);
-	
 	uint8_t recv_data[2];
+	write(file,buf,4);	
 	recv_data[0] = i2c_smbus_read_byte_data(file, 0x00);
 	recv_data[1] = i2c_smbus_read_byte_data(file, 0x01);
+	uint16_t data = recv_data[0]| recv_data[1]<<8;
 
 	printf("Device type: 0x%x 0x%x\n", recv_data[0], recv_data[1]);
+	printf("data is 0x%x\n",data);
 }
