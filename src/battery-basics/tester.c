26e63a81dcb3646a3d8700918f07bf1083b0a121
mjhegarty <mhegarty@gwu.edu>
2019-11-19 01:05:37 +0000

    Wrote basic i2c functions to access the battery babysitter data we are interested in.
    Other than looking at the device type(which still kinda confuses me), all the code does is
    read a specific register in I2C that stores a value we want. I made functions for voltage
    current state of charge and temperature with the register to read hard coded into them.
    For testing the values, voltage I compared to a multimeter reading the voltage so I
    know that one is correct. Current was at 0mA becuase nothing is plugged into it atm.
    Capacity seems higher than what it should be so im not sure about that one(might not be
    in hex). state of charge was at 50 which seems possible but if I recall it was at
    30ish when we were testing with the arduino so I will want to fully charge it then
    see if it is at 100% in hex or in int form. Temp was at around 295.2 Kelvin which is around
    room temp so that one is correct.

--- src/battery-basics/tester.c
+++ src/battery-basics/tester.c
@@ -11,8 +11,60 @@
 #include <sys/ioctl.h>
 #include <fcntl.h>
 #include <stdlib.h>
+uint16_t read_word(char word, int file){
+	uint8_t recv_data[2];
+	recv_data[0] = i2c_smbus_read_byte_data(file, word);
+	recv_data[1] = i2c_smbus_read_byte_data(file, word+1);
+	uint16_t data = recv_data[0]| recv_data[1]<<8;
+    return data;
+}
 
-void main(void) {
+//Checks to make sure device type is right
+int device_type(int file){
+	//TODO test that this works without the last 0x00 that we had before
+    char buf[] = {0x00, 0x01, 0x00};
+	write(file,buf,4);	
+	uint8_t recv_data[2];
+	recv_data[0] = i2c_smbus_read_byte_data(file, 0x00);
+	recv_data[1] = i2c_smbus_read_byte_data(file, 0x01);
+	uint16_t data = recv_data[0]| recv_data[1]<<8;
+	printf("Device type is %x\n", data);
+    if (data!=0x421){
+        return -1;
+    }
+    else{
+        return 0;
+    }
+}
+//Voltage in mV
+uint16_t read_voltage(int file){
+    char code = 0x04;
+    return read_word(code,file);
+}
+//avg current in mA
+//current is a signed int cuz it can be negative or positive
+int16_t read_current(int file){
+    char code= 0x10;
+    return (int16_t)read_word(code,file);
+}
+//remaining capacity in mAh
+uint16_t read_capacity(int file){
+    char code = 0x0C;
+    return read_word(code,file);
+}
+//state of charge unfiltered in percent
+uint16_t read_soc(int file){
+    char code=0x1C;
+    return read_word(code,file);
+}
+//temp of the battery in 0.1K 
+uint16_t read_temp(int file){
+    char code=0x02;
+    return read_word(code,file);
+}
+
+void print_test(void){
+	
 	const char *filename = "/dev/i2c-1";
 	int file = open(filename, O_RDWR);
 	if (file < 0) {
@@ -24,18 +76,22 @@ void main(void) {
 		printf("ioctl error: %s\n", strerror(errno));
 		exit(1);
 	}
-	char buf[10];
-	buf[0] = 0x00;
-	buf[1] = 0x01;
-	buf[2] = 0x00;
-	buf[3] = 0x00;
+    if (device_type(file)!=0){
+        printf("Device type doesn't match! i2c error");
+        exit(1);
+    }
+    uint16_t voltage = read_voltage(file);
+    int16_t current = read_current(file);
+    uint16_t capacity = read_capacity(file);
+    uint16_t state_of_charge = read_soc(file);
+    uint16_t temp = read_temp(file);
+    printf("Voltage is %i mV\n",voltage);
+    printf("Current is %i mA\n",current);
+    printf("Capacity is %i mAh\n",capacity);
+    printf("SoC is at %i percent\n",state_of_charge);
+    printf("Temp is %i 0.1K\n",temp);
+}
 
-	uint8_t recv_data[2];
-	write(file,buf,4);	
-	recv_data[0] = i2c_smbus_read_byte_data(file, 0x00);
-	recv_data[1] = i2c_smbus_read_byte_data(file, 0x01);
-	uint16_t data = recv_data[0]| recv_data[1]<<8;
-
-	printf("Device type: 0x%x 0x%x\n", recv_data[0], recv_data[1]);
-	printf("data is 0x%x\n",data);
+void main(void) {
+	print_test();
 }
