f0b78dc7399b88972f0731b51cc2c7a87393dc45
Zach Day <z@zach.gdn>
2020-03-13 20:13:01 +0000

    Document NNG helper macros

--- src/sunneed/src/shared/sunneed_ipc.h
+++ src/sunneed/src/shared/sunneed_ipc.h
@@ -13,14 +13,25 @@
 
 void (*_sunneed_nng_error_func)(const char *nng_call_name, int rv);
 
+/** 
+ * Sets the function to be called when an error is encountered during a SUNNEED_NNG_TRY_* macro.
+ * This function takes two arguments: a string representing the name of the function, and an integer containing the
+ *  return value of the failed NNG function call.
+ * Usually this function should somehow call `nng_strerror` to report NNG's description of the error.
+ */
 #define SUNNEED_NNG_SET_ERROR_REPORT_FUNC(FUNCNAME) \
     _sunneed_nng_error_func = FUNCNAME;
 
+/** Try to perform an NNG function, expanding to returning 1 if the NNG call fails. */
 #define SUNNEED_NNG_TRY_RET(CALL, ERROR_COND, ARGS...) { \
     int _rv; \
     SUNNEED_NNG_TRY_RET_SET(CALL, _rv, ERROR_COND, ## ARGS); \
 }
 
+/** 
+ * Try to perform an NNG function, expanding to returning 1 if the NNG call fails. 
+ * In the process of calling the function, the variable TARGET will be assigned its return value.
+ */
 #define SUNNEED_NNG_TRY_RET_SET(CALL, TARGET, ERROR_COND, ARGS...) { \
     if ((TARGET = CALL(ARGS)) ERROR_COND) { \
         _sunneed_nng_error_func(#CALL, TARGET); \
@@ -28,17 +39,20 @@ void (*_sunneed_nng_error_func)(const char *nng_call_name, int rv);
     } \
 }
 
+/** Try to perform an NNG function. */
 #define SUNNEED_NNG_TRY(CALL, ERROR_COND, ARGS...) { \
     int _rv; \
     SUNNEED_NNG_TRY_SET(CALL, _rv, ERROR_COND, ## ARGS); \
 }
     
+/**
+ * Try to perform an NNG function.
+ * In the process of calling the function, the variable TARGET will be assigned its return value.
+ */
 #define SUNNEED_NNG_TRY_SET(CALL, TARGET, ERROR_COND, ARGS...) { \
     if ((TARGET = CALL(ARGS)) ERROR_COND) { \
         _sunneed_nng_error_func(#CALL, TARGET); \
     } \
 }
-
-
 
 #endif
