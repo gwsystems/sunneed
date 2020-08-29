8178f7ddff98dfc8a6d83e51b9317cd16ee19727
jacobcannizzaro <jacobcannizzaro@gwu.edu>
2020-08-29 12:29:26 -0400

    accidently deleted c file, add c file & remove object file

---
+++ src/isolation-chamber/test_progs/hellosandbox.c
@@ -0,0 +1,27 @@
+#define _GNU_SOURCE
+#include <sched.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/utsname.h>
+#include <sys/wait.h>
+#include <unistd.h>
+
+static void print_nodename() {
+	struct utsname utsname;
+	uname(&utsname);
+	printf("%s\n", utsname.nodename);
+}
+
+int main(int argc, char *argv[]){
+
+	//printf("%s %s\n",argv[0], argv[1]);
+	printf("hello from sandbox\n");
+	//system("pwd");
+	//system("ls");
+	printf("PID in sandbox: %d\n", getpid());
+	printf("hostname in sandbox: ");
+	print_nodename();
+
+
+	return 0;
+}
